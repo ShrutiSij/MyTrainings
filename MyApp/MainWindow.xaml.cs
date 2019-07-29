999a07a62f4403149bb73276ad1b2d11e2469298
Shruti <shruti.engineering@mecmesin.com>
2019-07-29 11:21:10 +0100

    Changed PersonList to array

--- MyApp/MainWindow.xaml.cs
+++ MyApp/MainWindow.xaml.cs
@@ -6,20 +6,22 @@ namespace MyApp
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {
-        List<Person> Persons = new List<Person>();
+    {      
         public string boyImg = "/boy.png";
         public string girlImg = "/girl.png";
+        Person[] PersonArray = new Person[5];
+        //List<Person> PersonList = new List<Person>();
         public MainWindow()
         {
             SetData();
             InitializeComponent();
-            lstBox.ItemsSource = Persons;
+            lstBox.ItemsSource = PersonArray;
+            //lstBox.ItemsSource = PersonList;
         }
 
         public void SetData()
         {
-            Persons.Add(new Person
+            PersonArray[0] = new Person
             {
                 FirstName = "Ashutosh",
                 LastName = "Mor",
@@ -33,8 +35,9 @@ namespace MyApp
                     Street = "2 chequers court",
                     City = "Horsham"
                 }
-            });
-            Persons.Add(new Person
+            };
+
+            PersonArray[1] = new Person
             {
                 FirstName = "Amol",
                 LastName = "Sijaria",
@@ -48,8 +51,8 @@ namespace MyApp
                     Street = "12 chequers court",
                     City = "London"
                 }
-            });
-            Persons.Add(new Person
+            };
+            PersonArray[2] = new Person
             {
                 FirstName = "Shruti",
                 LastName = "Sijaria",
@@ -63,8 +66,8 @@ namespace MyApp
                     Street = "chequers court",
                     City = "Crawley"
                 }
-            });
-            Persons.Add(new Person
+            };
+            PersonArray[3] = new Person
             {
                 FirstName = "Varsha",
                 LastName = "Jain",
@@ -78,8 +81,8 @@ namespace MyApp
                     Street = "A Court",
                     City = "London"
                 }
-            });
-            Persons.Add(new Person
+            };
+            PersonArray[4] = new Person
             {
                 FirstName = "Niti",
                 LastName = "Jain",
@@ -93,37 +96,113 @@ namespace MyApp
                     Street = "82 chequers",
                     City = "Horsham"
                 }
-            });
-            Persons.Add(new Person
-            {
-                FirstName = "John",
-                LastName = "De",
-                Age = "34",
-                Hobby = "Badminton",
-                ImgSrc = boyImg,
-                Address = new Address
-                {
-                    Country = "UK",
-                    PinCode = "AH13 5TW",
-                    Street = "AB street",
-                    City = "Horsham"
-                }
-            });
-            Persons.Add(new Person
-            {
-                FirstName = "Peter",
-                LastName = "parker",
-                Age = "23",
-                Hobby = "Painting",
-                ImgSrc = boyImg,
-                Address = new Address
-                {
-                    Country = "UK",
-                    PinCode = "RH13 5RW",
-                    Street = "2 chequers court",
-                    City = "Horsham"
-                }
-            });
+            };
+
+            //PersonList.Add(new Person
+            //{
+            //    FirstName = "Ashutosh",
+            //    LastName = "Mor",
+            //    Age = "23",
+            //    Hobby = "Football",
+            //    ImgSrc = boyImg,
+            //    Address = new Address
+            //    {
+            //        Country = "UK",
+            //        PinCode = "RH13 5RW",
+            //        Street = "2 chequers court",
+            //        City = "Horsham"
+            //    }
+            //});
+            //PersonList.Add(new Person
+            //{
+            //    FirstName = "Amol",
+            //    LastName = "Sijaria",
+            //    Age = "23",
+            //    Hobby = "Reading",
+            //    ImgSrc = boyImg,
+            //    Address = new Address
+            //    {
+            //        Country = "UK",
+            //        PinCode = "RH14 5RW",
+            //        Street = "12 chequers court",
+            //        City = "London"
+            //    }
+            //});
+            //PersonList.Add(new Person
+            //{
+            //    FirstName = "Shruti",
+            //    LastName = "Sijaria",
+            //    Age = "22",
+            //    Hobby = "Cricket",
+            //    ImgSrc = girlImg,
+            //    Address = new Address
+            //    {
+            //        Country = "UK",
+            //        PinCode = "RH15 5RW",
+            //        Street = "chequers court",
+            //        City = "Crawley"
+            //    }
+            //});
+            //PersonList.Add(new Person
+            //{
+            //    FirstName = "Varsha",
+            //    LastName = "Jain",
+            //    Age = "45",
+            //    Hobby = "Writing",
+            //    ImgSrc = girlImg,
+            //    Address = new Address
+            //    {
+            //        Country = "UK",
+            //        PinCode = "RH18 5RW",
+            //        Street = "A Court",
+            //        City = "London"
+            //    }
+            //});
+            //PersonList.Add(new Person
+            //{
+            //    FirstName = "Niti",
+            //    LastName = "Jain",
+            //    Age = "34",
+            //    Hobby = "Tennis",
+            //    ImgSrc = girlImg,
+            //    Address = new Address
+            //    {
+            //        Country = "UK",
+            //        PinCode = "AB13 5RW",
+            //        Street = "82 chequers",
+            //        City = "Horsham"
+            //    }
+            //});
+            //PersonList.Add(new Person
+            //{
+            //    FirstName = "John",
+            //    LastName = "De",
+            //    Age = "34",
+            //    Hobby = "Badminton",
+            //    ImgSrc = boyImg,
+            //    Address = new Address
+            //    {
+            //        Country = "UK",
+            //        PinCode = "AH13 5TW",
+            //        Street = "AB street",
+            //        City = "Horsham"
+            //    }
+            //});
+            //PersonList.Add(new Person
+            //{
+            //    FirstName = "Peter",
+            //    LastName = "parker",
+            //    Age = "23",
+            //    Hobby = "Painting",
+            //    ImgSrc = boyImg,
+            //    Address = new Address
+            //    {
+            //        Country = "UK",
+            //        PinCode = "RH13 5RW",
+            //        Street = "2 chequers court",
+            //        City = "Horsham"
+            //    }
+            //});
         }
     }
 }
