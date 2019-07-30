2fb4b897b6317c8aa64b60bbd4a0f5acc88887b2
Shruti <shruti.engineering@mecmesin.com>
2019-07-30 16:11:08 +0100

    WPFMVVMPersonList

---
+++ WPFMVVMPersonList/Models/PersonModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using WPFMVVMPersonList.Enum;
+
+namespace WPFMVVMPersonList.Models
+{
+    public class PersonModel : INotifyPropertyChanged
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Age { get; set; }
+        public string Hobby { get; set; }
+        public Address Address { get; set; }
+        public Gender Gender { get; set; }
+        public string ImgSrc { get; set; }
+
+        public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this,
+                    new System.ComponentModel.PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
