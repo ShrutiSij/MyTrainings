1c977602d89aeb3bbab359252b15cd0c73d66d28
Shruti <shruti.engineering@mecmesin.com>
2019-07-31 10:07:49 +0100

    -

---
+++ WPFMVVMPersonList/MockData/MockPersonData.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections.Generic;
+using WPFMVVMPersonList.Models;
+namespace WPFMVVMPersonList.MockData
+{
+    public class MockPersonData
+    {
+        public static List<PersonModel> GetPersonData()
+        {
+            List<PersonModel> personList = new List<PersonModel>();
+            personList.Add(new PersonModel
+            {
+                FirstName = "Ashutosh",
+                LastName = "Mor",
+                Age = "23",
+                Hobby = "Football",
+                Gender = "Male",
+                Address = new Address
+                {
+                    Country = "UK",
+                    PinCode = "RH13 5RW",
+                    Street = "2 chequers court",
+                    City = "Horsham"
+                }
+            });
+            personList.Add(new PersonModel
+            {
+                FirstName = "Amol",
+                LastName = "Sijaria",
+                Age = "23",
+                Hobby = "Reading",
+                Gender = "Male",
+                Address = new Address
+                {
+                    Country = "UK",
+                    PinCode = "RH14 5RW",
+                    Street = "12 chequers court",
+                    City = "London"
+                }
+            });
+            personList.Add(new PersonModel
+            {
+                FirstName = "Shruti",
+                LastName = "Sijaria",
+                Age = "22",
+                Hobby = "Cricket",
+                Gender = "Female",
+                Address = new Address
+                {
+                    Country = "UK",
+                    PinCode = "RH15 5RW",
+                    Street = "chequers court",
+                    City = "Crawley"
+                }
+            });
+            personList.Add(new PersonModel
+            {
+                FirstName = "Varsha",
+                LastName = "Jain",
+                Age = "45",
+                Hobby = "Writing",
+                Gender = "Female",
+                Address = new Address
+                {
+                    Country = "UK",
+                    PinCode = "RH18 5RW",
+                    Street = "A Court",
+                    City = "London"
+                }
+            });
+            personList.Add(new PersonModel
+            {
+                FirstName = "Niti",
+                LastName = "Jain",
+                Age = "34",
+                Hobby = "Tennis",
+                Gender = "Female",
+                Address = new Address
+                {
+                    Country = "UK",
+                    PinCode = "AB13 5RW",
+                    Street = "82 chequers",
+                    City = "Horsham"
+                }
+            });
+            personList.Add(new PersonModel
+            {
+                FirstName = "John",
+                LastName = "De",
+                Age = "34",
+                Hobby = "Badminton",
+                Gender = "Male",
+                Address = new Address
+                {
+                    Country = "UK",
+                    PinCode = "AH13 5TW",
+                    Street = "AB street",
+                    City = "Horsham"
+                }
+            });
+            personList.Add(new PersonModel
+            {
+                FirstName = "Peter",
+                LastName = "parker",
+                Age = "23",
+                Hobby = "Painting",
+                Gender = "Male",
+                Address = new Address
+                {
+                    Country = "UK",
+                    PinCode = "RH13 5RW",
+                    Street = "2 chequers court",
+                    City = "Horsham"
+                }
+            });
+            return personList;
+        }
+    }
+}
--- WPFMVVMPersonList/ViewModels/PersonViewModel.cs
+++ WPFMVVMPersonList/ViewModels/PersonViewModel.cs
@@ -1,129 +1,19 @@
 ﻿using System.Collections.Generic;
-using WPFMVVMPersonList.Enum;
 using WPFMVVMPersonList.Models;
-
+using WPFMVVMPersonList.MockData;
 namespace WPFMVVMPersonList.ViewModels
 {
     public class PersonViewModel
-    {
-        public string boyImg = "/Resource/boy.png";
-        public string girlImg = "/Resource/girl.png";
-        public List<PersonModel> PersonList = new List<PersonModel>();
+    {        
         public List<PersonModel> Persons { get; set; }
 
         public PersonViewModel()
         {
             SetData();
-            Persons = PersonList;
         }
-
         private void SetData()
         {
-            PersonList.Add(new PersonModel
-            {
-                FirstName = "Ashutosh",
-                LastName = "Mor",
-                Age = "23",
-                Hobby = "Football",               
-                Gender = "Male",
-                Address = new Address
-                {
-                    Country = "UK",
-                    PinCode = "RH13 5RW",
-                    Street = "2 chequers court",
-                    City = "Horsham"
-                }
-            });
-            PersonList.Add(new PersonModel
-            {
-                FirstName = "Amol",
-                LastName = "Sijaria",
-                Age = "23",
-                Hobby = "Reading",               
-                Gender = "Male",
-                Address = new Address
-                {
-                    Country = "UK",
-                    PinCode = "RH14 5RW",
-                    Street = "12 chequers court",
-                    City = "London"
-                }
-            });
-            PersonList.Add(new PersonModel
-            {
-                FirstName = "Shruti",
-                LastName = "Sijaria",
-                Age = "22",
-                Hobby = "Cricket",                
-                Gender = "Female",
-                Address = new Address
-                {
-                    Country = "UK",
-                    PinCode = "RH15 5RW",
-                    Street = "chequers court",
-                    City = "Crawley"
-                }
-            });
-            PersonList.Add(new PersonModel
-            {
-                FirstName = "Varsha",
-                LastName = "Jain",
-                Age = "45",
-                Hobby = "Writing",
-                Gender = "Female",
-                Address = new Address
-                {
-                    Country = "UK",
-                    PinCode = "RH18 5RW",
-                    Street = "A Court",
-                    City = "London"
-                }
-            });
-            PersonList.Add(new PersonModel
-            {
-                FirstName = "Niti",
-                LastName = "Jain",
-                Age = "34",
-                Hobby = "Tennis",
-                Gender = "Female",
-                Address = new Address
-                {
-                    Country = "UK",
-                    PinCode = "AB13 5RW",
-                    Street = "82 chequers",
-                    City = "Horsham"
-                }
-            });
-            PersonList.Add(new PersonModel
-            {
-                FirstName = "John",
-                LastName = "De",
-                Age = "34",
-                Hobby = "Badminton",
-                Gender = "Male",
-                Address = new Address
-                {
-                    Country = "UK",
-                    PinCode = "AH13 5TW",
-                    Street = "AB street",
-                    City = "Horsham"
-                }
-            });
-            PersonList.Add(new PersonModel
-            {
-                FirstName = "Peter",
-                LastName = "parker",
-                Age = "23",
-                Hobby = "Painting",               
-                Gender = "Male",
-                Address = new Address
-                {
-                    Country = "UK",
-                    PinCode = "RH13 5RW",
-                    Street = "2 chequers court",
-                    City = "Horsham"
-                }
-            });
+            Persons = MockPersonData.GetPersonData();
         }
     }
 }
